c26d435e16e4d739ffd0bf55854156ef92eb52f9
SawshaDev <contactcallimarie@gmail.com>
2022-08-23 17:51:44 -0500

    a

--- modules/info/info.py
+++ modules/info/info.py
@@ -1,8 +1,10 @@
+import logging
 import hikari
 import crescent
 from utils import Context, Plugin
 
 plugin = Plugin()
+logger = logging.getLogger(__name__)
 
 @plugin.load_hook
 def on_load() -> None:
